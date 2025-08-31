1f120da9950f9cb52993f7575ffb038cd5d636ca
RUYANGA <ruyangam15@gmail.com>
2025-08-31 18:29:02 +0200

    input field

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -12,9 +12,8 @@ class CurrencyConvert extends StatelessWidget {
         title: const Text('Currency App', style: TextStyle(fontSize: 30)),
         centerTitle: true,
         backgroundColor: Colors.white,
-
       ),
-      
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -31,29 +30,43 @@ class CurrencyConvert extends StatelessWidget {
               "0",
               style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
             ),
-            TextField(
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.currency_exchange, size: 30,color: Colors.black,),
-                border: OutlineInputBorder(),
-                labelText: 'Amount',
-                hintText: 'Enter Amount', 
-                hintStyle: TextStyle(fontSize: 20,color: Colors.black),
-                labelStyle: TextStyle(fontSize: 22,color: Colors.black),
-                filled: true,
-                fillColor: const Color.fromARGB(255, 253, 252, 252),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
+
+            Padding(
+              padding: const EdgeInsets.all(22.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.currency_exchange,
+                    size: 30,
+                    color: const Color.fromARGB(255, 68, 65, 65),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 32, 32, 32),
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  labelText: 'Amount',
+                  hintText: 'Enter Amount',
+                  hintStyle: TextStyle(fontSize: 20, color: Colors.black),
+                  labelStyle: TextStyle(fontSize: 22, color: Colors.black),
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 253, 252, 252),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 128, 106, 106),
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: false,
                 ),
               ),
             ),
-            TextButton(onPressed: (){}, child: Text('Change',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 39
-              
-            ),
-            ))
           ],
         ),
       ),
