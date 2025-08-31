19702ae8915f2978cd387a8fe5626c22829b4244
RUYANGA <ruyangam15@gmail.com>
2025-08-31 14:19:04 +0200

    add textfield

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -6,20 +6,48 @@ class CurrencyConvert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Currency Converter')),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: BackButton(),
+        title: const Text('Currency App', style: TextStyle(fontSize: 30)),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+
+      ),
+      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Currency",
+              "Converter",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
                 color: Colors.deepPurpleAccent,
               ),
             ),
-            Text("0", style: TextStyle(fontSize: 48)),
+            Text(
+              "0",
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
+                hintText: 'Enter Amount',
+                hintStyle: TextStyle(fontSize: 20,color: Colors.black),
+                labelStyle: TextStyle(fontSize: 22,color: Colors.black),
+              ),
+            ),
+            TextButton(onPressed: (){}, child: Text('Change',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 39
+              
+            ),
+            ))
           ],
         ),
       ),
