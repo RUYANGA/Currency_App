bb07ccf00021a336913d64a633cf178c0ddc6736
RUYANGA <ruyangam15@gmail.com>
2025-09-01 14:18:35 +0200

    finish app

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,39 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConvert extends StatelessWidget {
+class CurrencyConvert extends StatefulWidget {
   const CurrencyConvert({super.key});
+
+  @override
+  State<CurrencyConvert> createState() => _CurrencyConvertState();
+}
+
+class _CurrencyConvertState extends State<CurrencyConvert> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        leading: BackButton(),
+        //leading: BackButton(),
         title: const Text('Currency App', style: TextStyle(fontSize: 30)),
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Converter",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurpleAccent,
-              ),
+              'Frw $result',
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
             ),
-            Text(
-              "0",
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            ),
-
             Padding(
               padding: const EdgeInsets.all(22.0),
               child: TextField(
+                controller: textEditingController,
                 decoration: InputDecoration(
                   suffixIcon: Icon(
                     Icons.currency_exchange,
@@ -48,7 +47,7 @@ class CurrencyConvert extends StatelessWidget {
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   labelText: 'Amount',
-                  hintText: 'Enter Amount',
+                  hintText: 'Enter USD',
                   hintStyle: TextStyle(fontSize: 20, color: Colors.black),
                   labelStyle: TextStyle(fontSize: 22, color: Colors.black),
                   filled: true,
@@ -61,9 +60,32 @@ class CurrencyConvert extends StatelessWidget {
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                 ),
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: true,
-                  signed: false,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(22.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.tryParse(textEditingController.text) != null
+                        ? double.parse(textEditingController.text) * 1450
+                        : 0;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+
+                child: Text(
+                  "Exchange",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                 ),
               ),
             ),
